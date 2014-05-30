b15b46a59e91309c2e0db3fd71ec24cd3fb956a1
uzimith <uzimith.x9@gmail.com>
2014-05-30 09:00:28 +0900

    make app job

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "rubygems"
 require_relative "lib/tweet"
+require_relative "lib/user"
  
 unless ENV['Heroku']
   require_relative "debug"
@@ -7,8 +8,16 @@ end
  
 module Clockwork
   handler do |job, at|
-    Tweet.new.tweet
+    case job
+    when 'frequent.job'
+      Tweet.new_mention do |mention|
+        User.create_by_tweet(mention)
+      end
+    when 'less.frequent.job'
+      Tweet.tweets
+    end
   end
  
-  every(30.seconds, 'frequent.job')
+  every(10.seconds, 'frequent.job')
+  every(1.minutes, 'less.frequent.job')
 end
